bd90bcfb8ba97e2abc5646f3ba4b6ab86110070a
Surplus_Up (2DU) <min08101@naver.com>
2018-08-05 23:04:58 +0900

    코드 추가 패치

--- namumark.php
+++ namumark.php
@@ -16,7 +16,7 @@
 	$wgHooks['ParserAfterTidy'][] = 'NamuMarkExtraHTML';
 
 	function j_print($data) {
-		echo '<script>console.log("'.preg_replace('/(\(|\)|\'|\"|\n)/', '\\$1', $data).'");</script>';
+		echo '<script>console.log("'.preg_replace('/(\(|\)|\'|\"|\n)/', '\\\$1', $data).'");</script>';
 	}
 
 	function table_parser($data, $cel_data, $start_data, $num = 0) {
@@ -104,7 +104,7 @@
 		if(preg_match("/<height=((?:(?!>).)*)>/", $data, $in_data)) {
 			$cel_style = $cel_style.'height: '.$in_data[1].'px;';
 		}
-			
+
 		$text_right = preg_match("/<\)>/", $data, $in_data_1);
 		$text_center = preg_match("/<:>/", $data, $in_data_2);
 		$text_left = preg_match("/<\(>/", $data, $in_data_3);
@@ -114,7 +114,7 @@
 			$cel_style = $cel_style.'text-align: center;';
 		} else if($text_left) {
 			$cel_style = $cel_style.'text-align: left;';
-		} else if($num == 0) { 
+		} else if($num == 0) {
 			if(preg_match("/^ /", $cel_data) && preg_match("/ $/", $cel_data)) {
 				$cel_style = $cel_style.'text-align: center;';
 			} else if(preg_match("/^ /", $cel_data)) {
@@ -151,7 +151,7 @@
 				$table = preg_replace('/\|\|\n?$/', "[nobr]".'|}'."[nobr]", $table);
 
 				while(true) {
-					if(preg_match('/\|\|\n((?:\|\|)+)((?:<(?:(?:(?!>).)+)>)*)\n*((?:(?!\|\||<\/td>).\n*)*)/', $table, $in_in_data)) {
+					if(preg_match('/\|\|\n((?:\|\|)+)((?:<(?:(?:(?!>).)+)>)*)\n*((?:(?!\|\||\[nobr\]\|).\n*)*)/', $table, $in_in_data)) {
 						$return_table = table_parser($in_in_data[2], $in_in_data[3], $in_in_data[1], $num);
 
 						$table = preg_replace('/\|\|\n((?:\|\|)+)((?:<(?:(?:(?!>).)+)>)*)\n*/', "[nobr]".'|- '.$return_table[1]."[nobr]".'| '.$return_table[2].' '.$return_table[3].' '.$return_table[4].' | ', $table, 1);
@@ -161,7 +161,7 @@
 				}
 
 				while(true) {
-					if(preg_match('/((?:\|\|)+)((?:<(?:(?:(?!>).)+)>)*)\n*((?:(?:(?!\|\||<\/td>).)|\n)*\n*)/', $table, $in_in_data)) {
+					if(preg_match('/((?:\|\|)+)((?:<(?:(?:(?!>).)+)>)*)\n*((?:(?:(?!\|\||\[nobr\]\|).)|\n)*\n*)/', $table, $in_in_data)) {
 						$return_table = table_parser($in_in_data[2], $in_in_data[3], $in_in_data[1], $num);
 
 						$table = preg_replace('/((?:\|\|)+)((?:<(?:(?:(?!>).)+)>)*)\n*/', "[nobr]".'| '.$return_table[2].' '.$return_table[3].' '.$return_table[4].' | ', $table, 1);
@@ -182,6 +182,7 @@
 	function NamuMark(&$parser, &$text, &$strip_state) {
 		$title = $parser -> getTitle();
 
+		$text = preg_replace('/\r/', "", $text);
 		$text = preg_replace('/\n/', "<br>", $text);
 		$text = "<br>".$text."<br>";
 
@@ -194,6 +195,108 @@
 			}
 		}
 
+		$text = preg_replace('/(?:<br>)( +)\|\|/', '<br>||', $text);
+		$text = preg_replace('/\|\|( +)(?:<br>)/', '||<br>', $text);
+
+		$text = preg_replace('/<!--((?:(?!-->).)+)-->/', '', $text);
+		$text = preg_replace('/<br>##(((?!<br>).)+)/', '<br>', $text);
+
+		$middle_stack = 0;
+		$middle_list = array();
+		$middle_number = 0;
+		$fol_num = 0;
+		while(true) {
+			$middle_r = '/(?:{{{((?:(?! |{{{|}}}).)*) ?|(}}}))/';
+			if(preg_match($middle_r, $text, $middle_data)) {
+				if(!isset($middle_data[2])) {
+					if($middle_stack > 0) {
+						$middle_stack += 1;
+
+						$text = preg_replace('/(?:{{{((?:(?! |{{{|}}}).)*)( ?)|(}}}))/', '<middle><middle><middle>'.$middle_data[1].'$1', $text, 1);
+					} else {
+						$middle_number += 1;
+
+						if(preg_match('/^(#|@|\+|\-)/', $middle_data[1])) {
+							if(preg_match('/^(#(?:[0-9a-f-A-F]{3}){1,2})/', $middle_data[1], $in_data)) {
+								array_push($middle_list, 'span');
+
+								$text = preg_replace($middle_r, '<span style="color: '.$in_data[1].';">', $text, 1);
+							} else if(preg_match('/^(?:#(\w+))/', $middle_data[1], $in_data)) {
+								array_push($middle_list, 'span');
+
+								$text = preg_replace($middle_r, '<span style="color: '.$in_data[1].';">', $text, 1);
+							} else if(preg_match('/^(?:@((?:[0-9a-f-A-F]{3}){1,2}))/', $middle_data[1], $in_data)) {
+								array_push($middle_list, 'span');
+
+								$text = preg_replace($middle_r, '<span style="background: #'.$in_data[1].';">', $text, 1);
+							} else if(preg_match('/^(?:@(\w+))/', $middle_data[1], $in_data)) {
+								array_push($middle_list, 'span');
+
+								$text = preg_replace($middle_r, '<span style="background: '.$in_data[1].';">', $text, 1);
+							} else if(preg_match('/^(\+|-)([1-5])/', $middle_data[1], $in_data)) {
+								array_push($middle_list, 'span');
+
+								if($in_data[1] == '+') {
+									$font_size = (string)((int)$middle_search[2] * 20 + 100);
+								} else {
+									$font_size = (string)(100 - (int)$middle_search[2] * 10);
+								}
+
+								$text = preg_replace($middle_r, '<span style="font-size: '.$font_size.'%;">', $text, 1);
+							} else if(preg_match('/^#!wiki/', $middle_data[1], $in_data)) {
+								$wiki_r = '/{{{#!wiki(?: style=(?:&quot;|&#x27;)((?:(?!&quot;|&#x27;).)*)(?:&quot;|&#x27;))?\n?/';
+								if(preg_match($wiki_r, $text, $$middle_data_2)) {
+								} else {
+									$$middle_data_2 = array('', '');
+								}
+								
+								array_push($middle_list, 'div_end');
+
+								$text = preg_replace($wiki_r, '<div id="wiki_div" style="'.(string)$middle_data_2[1] + '">', $text, 1);
+							} else {
+								array_push($middle_list, 'span');
+
+								$text = preg_replace($middle_r, '<span>'.$middle_data[1], $text, 1);
+							}
+						} else {
+							array_push($middle_list, 'nowiki');
+							$middle_stack += 1;
+
+							$text = preg_replace($middle_r, '<nowiki>'.$middle_data[1], $text, 1);
+						}
+					}
+				} else {
+					if(count($middle_list) == 0) {
+						$text = preg_replace($middle_r, '</middle></middle></middle>', $text, 1);
+					} else {
+						if($middle_stack > 0) {
+							$middle_stack -= 1;
+						}
+
+						if($middle_stack > 0) {
+							$text = preg_replace($middle_r, '</middle></middle></middle>', $text, 1);
+						} else {
+							if($middle_number > 0) {
+								$middle_number -= 1;
+							}
+
+							if($middle_list[$middle_number] == '2div') {
+								$text = preg_replace($middle_r, '</div_end></div_end></div_end>', $text, 1);
+							} else if($middle_list[$middle_number] == 'pre') {
+								$text = preg_replace($middle_r, '</code></pre>', $text, 1);
+							} else {
+								$text = preg_replace($middle_r, '</'.$middle_list[$middle_number].'>', $text, 1);
+							}
+
+							array_splice($middle_list, $middle_number, 1);
+						}
+					}
+				}
+			} else {
+				break;
+			}
+		}
+
 		$text = preg_replace('/__((?:(?!__).)+)__/', '<u>$1</u>', $text);
 		$text = preg_replace('/--((?:(?!--).)+)--/', '<s>$1</s>', $text);
 		$text = preg_replace('/~~((?:(?!~~).)+)~~/', '<s>$1</s>', $text);
@@ -201,8 +304,6 @@
 		$text = preg_replace('/,,((?:(?!,,).)+),,/', '<sub>$1</sub>', $text);
 
 		$text = preg_replace('/\[목차\]/', '__TOC__', $text);
-
-		$text = preg_replace('/\[br\]/', "<br>", $text);
 
 		$text = preg_replace('/\[date\]/', date("Y-m-d H:i:s", time()), $text);
 		
@@ -234,14 +335,14 @@
 				$block_data = preg_replace('/(?:<br>)> ?/', "<br>", $block_data);
 				$block_data = preg_replace('/(?:<br>)$/', '', $block_data);
 
-				$text = preg_replace($block_r, "<br>".'<blockquote>'.$block_data.'</blockquote>'."<br>", $text, 1);
+				$text = preg_replace($block_r, "<br><blockquote>'.$block_data.'</blockquote><br>", $text, 1);
 			} else {
 				break;
 			}
 		}
 
 		while(true) {
-			$list_r = '/((?:<br>)(?:(?: *)\* ?(?:(?:(?!(?:<br>)).)+)(?:<br>))+)/';
+			$list_r = '/((?:(?:<br>|\[nobr\]))(?:(?: *)\* ?(?:(?:(?!(?:<br>)).)+)(?:<br>))+)/';
 			if(preg_match($list_r, $text, $in_data)) {
 				$li_data = $in_data[1];
 				
@@ -266,9 +367,12 @@
 		}
 
 		$text = preg_replace('/<star>/', '*', $text);
+		$text = preg_replace('/<\/div_end>/', '</div>', $text);
+		$text = preg_replace('/<middle>/', '{', $text);
+		$text = preg_replace('/<\/middle>/', '}', $text);
 		
 		while(true) {
-			$indent_r = '/(?:<br>)( +)/';
+			$indent_r = '/(?:(?:<br>|\[nobr\]))( +)/';
 			if(preg_match($indent_r, $text, $in_data)) {
 				$data_len = mb_strlen($in_data[1], 'utf-8');
 
@@ -278,16 +382,29 @@
 			}
 		}
 
+		$text = preg_replace('/\[br\]/', "\n", $text);
 		$text = preg_replace('/(?:<br>)/', "\n", $text);
 		$text = table_start($text);
 		$text = preg_replace('/\n/', "<br>", $text);
 
-		$text = preg_replace('/\[\[(http(?:s):\/\/(?:[^|]+))\|?((?:(?!\]\]).)+)\]\]/', '[$1 $2]', $text);
+		$text = preg_replace('/\[nobr\]/', "\n", $text);
+
+		$text = preg_replace('/\]\]/', "<link>", $text);
+		$text = preg_replace('/\[\* ([^\]]+)\]/', "<ref>$1</ref>", $text);
+
+		$text = preg_replace('/<link>/', "]]", $text);
+
+		j_print($text);
+		$text = preg_replace('/\[\[(http(?:s)?:\/\/(?:[^|]+))\|?((?:(?!\]\]).)+)\]\]/', '[$1 $2]', $text);
+
+		$text = preg_replace('/(?:<br>)(={1,6})([^=]+)={1,6}(?:<br>)/', "\n$1$2$1\n", $text);
+		$text = preg_replace('/(?:\n)(={1,6})([^=]+)={1,6}(?:<br>)/', "\n$1$2$1\n", $text);
+		$text = preg_replace('/(?:<br>)(={1,6})([^=]+)={1,6}(?:\n)/', "\n$1$2$1\n", $text);
 
 		$text = preg_replace('/^(?:<br>)+/', '', $text);
 		$text = preg_replace('/(?:<br>)+$/', '', $text);
-		
-		$text = preg_replace('/\[nobr\]/', "\n", $text);
+
+		$text = preg_replace('/class="warningbox"><br>/', 'class="warningbox">', $text);
 	}
 
 	function NamuMarkHTML(Parser &$parser, &$text) {
